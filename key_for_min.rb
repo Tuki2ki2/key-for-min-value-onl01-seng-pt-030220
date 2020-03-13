32c157075b54e9c063bb7577f0e865e9fb4dac38
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-13 16:42:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@ def key_for_min_value(hash)
 small = nil
 name = nil
 hash.each do |key, value|
-  if small == nil || small < value
+  if small == nil || value < small
+    
     small = value
     name = key
   end
